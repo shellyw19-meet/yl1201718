36cd3e3ba0be9fd731defb8c42d63f3c1efd55c5
adan19-meet <adan19@meet.mit.edu>
2017-10-29 19:27:03 +0200

    shelly is the best in session number3 and musa helped her

---
+++ TurtleShape.py
@@ -0,0 +1,26 @@
+import turtle
+turtle.speed(100)
+turtle.pensize(10)
+# turtle.bgcolor("black")
+
+# for i in range(1):
+# 	turtle.pencolor("blue")
+# 	turtle.forward(250)
+# 	turtle.right(144)
+# 	turtle.pencolor("green")
+# 	turtle.forward(250)
+# 	turtle.right(144)
+# 	turtle.pencolor("pink")
+# 	turtle.forward(250)
+# 	turtle.right(144)
+# 	turtle.pencolor("white")
+# 	turtle.forward(250)
+# 	turtle.right(144)
+# 	turtle.pencolor("red")
+# 	turtle.forward(250)
+# 	turtle.right(144)
+# 	turtle.pencolor("yellow")
+# turtle.mainloop()
+
+
+turtle.goto
