e7ac818721b22e78975ac511c928f53247775fc9
adan19-meet <adan19@meet.mit.edu>
2018-03-01 18:32:35 +0200

    hiwfuw

---
+++ basketballcourt.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.setup( width = 800, height = 550, startx = None, starty = None)
+
+turtle.bgpic("court2.gif")
+
+turtle.addshape("goodbasket.gif")
+turtle.shape("goodbasket.gif")
+turtle.goto(320,0)
+
+
+turtle.mainloop()
+
